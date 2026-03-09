1e95318af5c851324f4a64ab80701998f50aabc3
Gecko <geemuzuki0803@gmail.com>
2026-03-09 22:02:06 +0900

    Add files via upload

--- services/createCurrencyList.js
+++ services/createCurrencyList.js
@@ -1,12 +1,17 @@
 import localStorageData from '../assets/data/localStorageData.js';
+import currencyData from '../assets/data/currencies.json';
 
 function createCurrencyList() {
     const currencyListElement = document.getElementById('currency-list');
     currencyListElement.innerHTML = '';
 
-    localStorageData.forEach(currency => {
+    localStorageData.forEach(code => {
         const li = document.createElement('li');
-        li.textContent = currency;
+        const flagCode = currencyData[code]?.flag || '';
+        li.innerHTML = `
+            <span class="fi fi-${flagCode}"></span>
+            <span class="currency-code">${code}</span>
+        `;
         currencyListElement.appendChild(li);
     });
 }
