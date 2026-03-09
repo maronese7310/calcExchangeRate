0e9cea2f0d8ca54fb0e44ed0b03c3352cb14510a
Gecko <geemuzuki0803@gmail.com>
2026-03-09 20:54:19 +0900

    Add files via upload

--- assets/script/main.js
+++ assets/script/main.js
@@ -10,10 +10,12 @@ async function initialProcess() {
     // 1) 為替レートの取得
     const rateData = await fetchRate();
 
+
     // 2) データの加工
     rates.fetchDate = formatTimestamp(rateData.timestamp);
 
     rates.exchangeRate = rateData.rates;
+
 
     // 3) メイン画面へ遷移
     const startScreen = document.getElementById("start-screen");
@@ -21,23 +23,4 @@ async function initialProcess() {
 
     startScreen.classList.add("hidden");
     mainScreen.classList.remove("hidden");
-}
-
-/*
-document.addEventListener("DOMContentLoaded", () => {
-
-    const startScreen = document.getElementById("start-screen")
-    const mainScreen = document.getElementById("main-screen")
-
-    function showMainScreen() {
-
-        startScreen.classList.add("hidden")
-        mainScreen.classList.remove("hidden")
-
-        console.log("done")
-    }
-
-    setTimeout(showMainScreen, 1000)
-
-})
-*/
+};
