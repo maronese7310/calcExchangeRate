0fc62053b050075bbfcf9d246faee0d1c60ce7da
Gecko <geemuzuki0803@gmail.com>
2026-03-09 22:04:48 +0900

    Add files via upload

--- services/createCurrencyList.js
+++ services/createCurrencyList.js
@@ -1,15 +1,17 @@
 import localStorageData from '../assets/data/localStorageData.js';
-import currencyData from '../assets/data/currencies.json';
 
-function createCurrencyList() {
+async function createCurrencyList() {
+    const response = await fetch('assets/data/currencies.json');
+    const currencyData = await response.json();
+
     const currencyListElement = document.getElementById('currency-list');
     currencyListElement.innerHTML = '';
 
     localStorageData.forEach(code => {
+        const className = currencyData[code]?.flag || '';
         const li = document.createElement('li');
-        const flagCode = currencyData[code]?.flag || '';
         li.innerHTML = `
-            <span class="fi fi-${flagCode}"></span>
+            <span class="fi fi-${className}"></span>
             <span class="currency-code">${code}</span>
         `;
         currencyListElement.appendChild(li);
