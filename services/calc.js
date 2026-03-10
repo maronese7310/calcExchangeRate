3a4c389c885b88e00c96fa1163777bce143597d1
Gecko <geemuzuki0803@gmail.com>
2026-03-11 00:21:13 +0900

    Add files via upload

--- services/calc.js
+++ services/calc.js
@@ -1,8 +1,9 @@
 import { rates } from "../assets/data/rates.js";
+import { selectedCurrency } from "../assets/data/localStorageData.js";
 
 function calcExchangeRate(code, amount) {
     const targetRate = rates.exchangeRate[code];
-    const exchangeRate = targetRate / rates.exchangeRate["JPY"];
+    const exchangeRate = targetRate / rates.exchangeRate[selectedCurrency[0]];
     return amount * exchangeRate;
 }
 
