6f94b3c55b98345d2131d5c9934db007b571a8c8
Gecko <geemuzuki0803@gmail.com>
2026-03-11 22:55:17 +0900

    Add files via upload

---
+++ assets/styles/pages/main.js
@@ -0,0 +1,36 @@
+import fetchRate from "../../services/fetchRate.js";
+import formatTimestamp from "../../services/formatter.js";
+import { rates } from "../data/rates.js";
+import createCurrencyList from "../../services/createCurrencyList.js";
+
+document.addEventListener("DOMContentLoaded", async () => {
+    await initialProcess();
+});
+
+async function initialProcess() {
+    // 1) 為替レートの取得
+    const rateData = await fetchRate();
+
+
+    // 2) データの加工
+    rates.fetchDate = formatTimestamp(rateData.timestamp);
+
+    rates.exchangeRate = rateData.rates;
+    rates.exchangeRate["JPY"] = 1; // 基準通貨としてJPYを追加
+
+
+    // 3) メイン画面へ遷移
+    const rateTimeElement = document.getElementById("rate-time");
+    if (rateTimeElement && rates && rates.fetchDate) {
+        rateTimeElement.textContent = rates.fetchDate;
+    }
+
+    const startScreen = document.getElementById("start-screen");
+    const mainScreen = document.getElementById("main-screen");
+
+    startScreen.classList.add("hidden");
+    mainScreen.classList.remove("hidden");
+
+    // 4) 通貨リストの作成
+    await createCurrencyList();
+};
